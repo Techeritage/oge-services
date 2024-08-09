bd8e0a190a02af2f53ba9accc4a68a57ed7da08d
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-08-09 13:28:00 +0100

    update

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -4,14 +4,19 @@ export default function ServicesPage() {
   return (
     <main className="min-h-[100vh] py-20 px-[3%]">
       <h1 className="headingBig lg:text-5xl text-center mx-auto">
-        Comprehensive <span className="text-primary"> Financial Services</span>{" "}
-        Tailored to Your Needs
+        Expert <span className="text-primary">Financial and Business</span>{" "}
+        Services for Your Success
       </h1>
       <p className="lg:max-w-[50%] text-center mx-auto mt-3 text-xs lg:text-sm">
-        At OGE Services, we offer a range of financial solutions designed to
-        meet your unique needs. Explore our services to find out how we can
-        support your financial goals with professionalism and expertise.
+        At OGE Services, we provide a comprehensive suite of services designed
+        to address all your financial, regulatory, and business management
+        needs. Explore our offerings to discover how we can support your
+        organization with expertise and dedication.
       </p>
+
+      <div>
+        
+      </div>
     </main>
   );
 }
