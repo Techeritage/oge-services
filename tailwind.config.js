ce1e85770a128b10db85e0b92bad0a17b5c610f3
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-08-14 09:19:51 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        primary: "#c70e10",
+        primary: "#972620",
       },
     },
     fontFamily: {
